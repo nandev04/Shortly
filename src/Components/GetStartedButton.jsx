9ebdfc244e0e2c52a88fed4b92eec8fe315803fc
Renan Alves <renanlvsdv@gmail.com>
2024-01-26 03:39:28 -0300

    sloganFooter posicionado, alterações no position do advancedContainer

---
+++ src/Components/GetStartedButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '../styleComponents/GetStartedButton.module.css';
+
+const GetStartedButton = () => {
+  return <button className={style.getStarted}>Get Started</button>;
+};
+
+export default GetStartedButton;
---
+++ src/Components/SloganFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../styleComponents/SloganFooter.module.css';
+
+const SloganFooter = () => {
+  return (
+    <div className={style.sloganContainer}>
+      <h2 className={style.sloganTitle}>Boost your links today</h2>
+    </div>
+  );
+};
+
+export default SloganFooter;
