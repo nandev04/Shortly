89220c1f8a008211ae8833316a178c625a0f7b05
Renan Alves <renanlvsdv@gmail.com>
2024-03-12 03:38:42 -0300

    menu mobile finalizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,17 +5,41 @@ import Button from './Button';
 const Header = () => {
   return (
     <header className={style.header}>
-      <div className={style.navigation}>
+      <div className={style.menu}>
         <h1 className={style.logoSite}>Shortly</h1>
-        <ul className={style.navigationButtons}>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Resources</li>
-        </ul>
+        <div className={style.navigation}>
+          <ul className={style.navigationButtons}>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Resources</li>
+          </ul>
+        </div>
+        <div>
+          <button className={style.login}>Login</button>
+          <Button title={'Sign Up'} border={`${20}px`} />
+        </div>
       </div>
-      <div>
-        <button className={style.login}>Login</button>
-        <Button title={'Sign Up'} border={`${20}px`} />
+      <div className={style.menuToggle}>
+        <h1 className={style.logoSite}>Shortly</h1>
+        <div className={style.gripPosition}>
+          <div className={style.gripButton}>
+            <button
+              className={style.dropMenuButton + ' btn'}
+              onClick={() => {
+                const list = document.querySelector('.dropMenu');
+                list.classList.toggle('activeDrop');
+              }}
+            ></button>
+          </div>
+
+          <ul className={style.navigationButtonsMenu + ' dropMenu'}>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Resources</li>
+            <button className={style.loginMenu}>Login</button>
+            <button className={style.signUp}>Sign Up!</button>
+          </ul>
+        </div>
       </div>
     </header>
   );
